230d5ba0cc78aebf5e63a09b7819c1e3d80e4d5e
Jose <jose.muhlanga@student.moringaschool.com>
2023-04-01 16:40:29 +0300

    Creation of Comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+    before_action :require_login
+  
+    def create
+      @book = Book.find(params[:book_id])
+      @comment = current_user.comments.build(comment_params.merge(book_id: @book.id))
+      if @comment.save
+        redirect_to @book, notice: 'Comment added successfully!'
+      else
+        @comments = @book.comments.reload
+        render 'books/show'
+      end
+    end
+  
+    def destroy
+      @comment = current_user.comments.find(params[:id])
+      @comment.destroy
+      redirect_to @comment.book, notice: 'Comment deleted successfully!'
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+
 end
