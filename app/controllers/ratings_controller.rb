5c46dd9f2331982fb034a4490800cf6b0d4340d5
Jose <jose.muhlanga@student.moringaschool.com>
2023-04-01 16:41:41 +0300

    Creation of Ratings controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,35 @@
 class RatingsController < ApplicationController
+    before_action :require_login
+  
+    def create
+      @book = Book.find(params[:book_id])
+      @rating = current_user.ratings.build(rating_params.merge(book_id: @book.id))
+      if @rating.save
+        redirect_to @book, notice: 'Rating added successfully!'
+      else
+        redirect_to @book, alert: 'Failed to add rating!'
+      end
+    end
+  
+    def update
+      @rating = current_user.ratings.find(params[:id])
+      if @rating.update(rating_params)
+        redirect_to @rating.book, notice: 'Rating updated successfully!'
+      else
+        redirect_to @rating.book, alert: 'Failed to update rating!'
+      end
+    end
+  
+    def destroy
+      @rating = current_user.ratings.find(params[:id])
+      @rating.destroy
+      redirect_to @rating.book, notice: 'Rating deleted successfully!'
+    end
+  
+    private
+  
+    def rating_params
+      params.require(:rating).permit(:value)
+    end
+
 end
