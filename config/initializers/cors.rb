22ab660f3f48662d0856286704092d404c6ff95b
Jose <jose.muhlanga@student.moringaschool.com>
2023-04-02 18:42:00 +0300

    Configuring CORS for my system again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://main--zingy-manatee-8fbde9.netlify.app'
+      origins 'https://main--zingy-manatee-8fbde9.netlify.app, http://localhost:3000'
   
       resource '*',
         headers: :any,
